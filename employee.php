23ad6ca9ad281e8849a58472dc535405f060f1e4
rosmejohn <rosme@mwttech.com>
2021-04-15 14:56:54 +0530

    first commit

---
+++ employee.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Employee Details</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php session_start();
+include 'config/database.php';?>
+<style>
+.table-primary{background-color: #b8daff;}
+.file {
+  position: relative;
+  overflow: hidden;
+}
+.table{margin-top:15px;}
+.file_upload {
+  position: absolute;
+  font-size: 50px;
+  opacity: 0;
+  right: 0;
+  top: 0;
+}
+h4{color:#777;}
+</style>
+
+<?php
+$_SESSION["error_status"] ='0';
+if(isset($_FILES['file']['name'])){
+  //file validation
+  $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+  if($ext=='csv'){
+  //Row and Column Validation
+  $row = 1;
+  $error_status =0 ;
+  $handle = fopen($_FILES['file']['tmp_name'], "r");
+    while ($data = fgetcsv($handle)) {
+      $column = count($data);//column count
+      $row++;//row count
+      if($row>20)
+      {
+        $flag = 1;
+        // 'Specified record limit exceeds!'; 
+      }
+      if($column!=5)
+      {
+        $flag = 1;
+        // 'Specified column limit exceeds!'; 
+      }
+      if($flag==0){
+        $emp_code = $data[0];
+        $emp_name = $data[1];  
+        $department = $data[2];
+        $age = $data[3];
+        $experience = $data[4];
+        if(preg_match('/[^a-z_\-0-9]/i', $emp_code))
+        {
+          // 'Employee Code is Invalid Input-alphanumeric validation';
+          $error_status =1;
+        }
+        if (preg_match("/[^A-Za-z]/", $emp_name))
+        {
+          //'Employee Name is Invalid Input-character validation';
+          $error_status =1;
+        }
+        if (preg_match("/[^A-Za-z]/", $department))
+        {
+         // 'Department is Invalid Input-character validation';
+          $error_status =1;
+        }
+        if(!is_numeric($experience))
+        {
+          // 'Experience is Invalid Input-numeric validation';
+          $error_status =1;
+        }
+        if(!is_numeric($age))
+        {
+        //'Age is Invalid Input-numeric validation';
+        $error_status =1;
+        }
+    }
+    else {
+      $_SESSION["file_validation"] = "1";
+    }
+  }
+
+  if($error_status ==1)
+  {
+    $_SESSION["error_status"] = "1";
+  }
+  elseif($flag==0){
+    $_SESSION["error_status"] ="2";
+    $fhandle = fopen($_FILES['file']['tmp_name'], "r");
+      while ($listing = fgetcsv($fhandle)) {
+      $emp_code = $listing[0];
+      $emp_name = $listing[1];  
+      $department = $listing[2];
+      $age = $listing[3];
+      $experience = $listing[4];
+      $sql = "INSERT INTO employee_details(employee_code,employee_name,department,age,experience)
+      VALUES ('$emp_code','$emp_name','$department',$age,$experience)";
+        $conn->query($sql);
+  }
+  }
+  }
+  else{
+    $_SESSION["file_status"] ="1";
+  } 
+} 
+?>
+<div class="container">
+  <h1>Employee Management</h1>
+  <h4>You can add employee details by uploading csv files.</h4>
+  <?php if(isset($_SESSION["error_status"]) && ($_SESSION["error_status"]==1)){ ?>
+  <div class="alert alert-danger alert-dismissible">
+    <strong>Error!</strong> Invalid Data format.please check before uploading.
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+  </div>
+  <?php } if(isset($_SESSION["error_status"]) && ($_SESSION["error_status"]==2)){ ?>
+  <div class="alert alert-success alert-dismissible">
+    <strong>File Uploaded Successfully.</strong>
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+  </div> 
+  <?php }  if(isset($_SESSION["file_status"]) && ($_SESSION["file_status"]==1)){ ?>
+  <div class="alert alert-danger alert-dismissible">
+    <strong>Invalid File Format !</strong>Allowed file type CSV.
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+  </div> 
+  <?php }  if(isset($_SESSION["file_validation"]) && ($_SESSION["file_validation"]==1)){ ?>
+  <div class="alert alert-danger alert-dismissible">
+    <strong>Invalid File Data Format !</strong>Should contain minimum of 5 rows and maximum of 20 records.
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+  </div> 
+  <?php }?>
+  <form id="myform" method="post" action="" enctype="multipart/form-data" id="myform">
+  <div class='row'>
+  <div class='col-md-4 col-xs-12 pull-right'>
+  <div class='pull-right'>
+  <div class="file btn btn-sm btn-primary">
+
+		<input type="file" name="file"  id='file' >
+	</div>
+    <input type="submit" value="Upload" class="file btn btn-sm btn-success" />
+  </div>  
+  </div>  
+  </div>  
+</form>
+<table class="table table-bordered table-striped">
+    <thead>
+      <tr  class="table-primary">
+        <th>Employee Code</th>
+        <th>Employee Name</th>
+        <th>Department</th>
+        <th>Age</th>
+        <th>Experience in the Organization</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php
+    $sql = "SELECT * FROM employee_details";
+    $employee = $conn->query($sql);
+     foreach($employee as $emp){ ?>
+      <tr>
+        <td><?= $emp['employee_code']; ?></td>
+        <td><?= $emp['employee_name']; ?></td>
+        <td><?= $emp['department']; ?></td>
+        <td><?= $emp['age']; ?></td>
+        <td><?= $emp['experience']; ?></td>
+      </tr>
+      <?php } ?>
+    </tbody>
+</table>
+</div>
+</body>
+
+<script type="text/javascript">
+$(document).ready(function () {
+  setTimeout(function() { 
+    $('.alert-dismissible').hide();
+    '<?php unset($_SESSION['error_staus']);
+           unset($_SESSION['file_status']);
+           unset($_SESSION['file_validation']);
+        ?>'
+    }, 6000);
+});
+</script>
+</html>
+
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Employee Management</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif;inheight:100%}
+.w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+.fa-anchor,.fa-coffee {font-size:200px}
+section.w3-center{padding:128px 16px;min-height:100vh}
+.w3-container{background-color: #607d8b!important;color: #fff!important;}
+.w3-button{color: #fff!important;background-color: #3f51b5!important;}
+</style>
+<body>
+<!-- welcome page -->
+<section class="w3-container  w3-center" >
+  <h1 class="w3-margin w3-jumbo">Employee Management</h1>
+
+  <a class="w3-button  w3-padding-large w3-large w3-margin-top" href="employee.php">Welcome</a>
+</section>
+</body>
+</html>
